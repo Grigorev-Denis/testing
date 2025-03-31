ba16ad553f7ded8ff0807d5e8f16ddc6148e53f0
Grigorev-Denis <freestylemilll@gmail.com>
2025-03-31 22:18:59 +0300

    module6

--- module6/src/Main.java
+++ module6/src/Main.java
@@ -1,13 +1,14 @@
-public static void main(String[] args) {
-    String[]arr = {"привет","ПаЛкА","гипербола"};
-    String secondWord = arr[1];
-    StringBuilder builder = new StringBuilder(secondWord);
-    builder.insert(3, " ");
-    builder.replace(1,2, "О");
-    String result = builder.toString();
+public class Main {
+    public static void main(String[] args) {
+        String[]arr = {"привет","ПаЛкА","гипербола"};
+        String secondWord = arr[1];
+        StringBuilder builder = new StringBuilder(secondWord);
+        builder.insert(3, " ");
+        builder.replace(1,2, "О");
+        String result = builder.toString();
 
-    System.out.println(arr[0].toUpperCase());
-    System.out.println(arr[1].toLowerCase());
-    System.out.println(result);
-}
+        System.out.println(arr[0].toUpperCase());
+        System.out.println(arr[1].toLowerCase());
+        System.out.println(result);
+    }
 }
