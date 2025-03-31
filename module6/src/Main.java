82443f04fd593d135f47ead0c2ef873ccd2dbadd
Grigorev-Denis <freestylemilll@gmail.com>
2025-03-31 22:12:50 +0300

    module6

---
+++ module6/src/Main.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+    String[]arr = {"привет","ПаЛкА","гипербола"};
+    String secondWord = arr[1];
+    StringBuilder builder = new StringBuilder(secondWord);
+    builder.insert(3, " ");
+    builder.replace(1,2, "О");
+    String result = builder.toString();
+
+    System.out.println(arr[0].toUpperCase());
+    System.out.println(arr[1].toLowerCase());
+    System.out.println(result);
+}
+}
