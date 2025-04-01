0f9ff29e5bbc74ccce21385f35232ced3f2cd657
Grigorev-Denis <freestylemilll@gmail.com>
2025-04-01 16:17:59 +0300

    исправил модуль6

--- module6/src/Main.java
+++ module6/src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        String[]arr = {"привет","ПаЛкА","гипербола"};
-        String secondWord = arr[1];
-        StringBuilder builder = new StringBuilder(secondWord);
-        builder.insert(3, " ");
-        builder.replace(1,2, "О");
-        String result = builder.toString();
+        String[]arr = {"привет","ПоЛкА","гипербола"};
+        String secondWord ;
+        String result = "Слов с буквой 'О' не найдено";
+        for (String word: arr){
+            char letter = word.charAt(1);
+            if(letter=='О'||letter=='о'){
+                secondWord = word;
+                StringBuilder builder = new StringBuilder(secondWord);
+                builder.insert(3, " ");
+                builder.replace(1,2, "О");
+                result = builder.toString();
+                break;
+            }
+
+        }
 
         System.out.println(arr[0].toUpperCase());
         System.out.println(arr[1].toLowerCase());
