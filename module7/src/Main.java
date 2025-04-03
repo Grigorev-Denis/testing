108a782ce55dc831427087b5943c643a413287d4
Grigorev-Denis <freestylemilll@gmail.com>
2025-04-03 21:38:32 +0300

    модуль 7

--- module7/src/Main.java
+++ module7/src/Main.java
@@ -3,12 +3,12 @@ public class Main {
     public static void main(String[] args) {
 Developer dev = new Developer();
 dev.setName("Игорь");
-dev.setSalary(150000.50f);
+dev.setSalary(150000.99f);
 dev.work();
 
 Manager man = new Manager();
 man.setName("Павел");
-man.setSalary(180000.70f);
+man.setSalary(180000.99f);
 man.work();
 
 
